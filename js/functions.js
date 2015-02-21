49cc3bec028f4b7e2159fdc6c08e71627c23b4fe
kiranw <kiranw@mit.edu>
2015-02-21 18:59:41 -0500

    form safety

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,70 @@
+var email;
+var startloc;
+var endLat;
+var endLong;
+var itsOkay=false;
+
 function hideSplash() {
 	$("#splash").hide();
-	console.log("hiding")
 };
+
+
+function coord() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    } else {
+	        $("#mylocationerror").show();
+	    }
+}
+	
+
+function showPosition(position) {
+		startloc=[position.coords.latitude,position.coords.longitude]
+		console.log("getting");
+		itsOkay=true;
+		$("#startloc").val("Position Recorded");
+	}
+
+
+function submit() {
+	$('.error').hide();
+	
+
+	email = $("input[name=email]").val();
+	console.log("email given: "+ email)
+      if (email === "") {
+        $("#email_error").show();
+        $("input#email").focus();
+        return false;
+      }
+
+	if (/^[0-9A-Za-z]+@.$/.test(email)) {
+		console.log("no invalid character")
+		if (email.indexOf('@') > -1) {
+		       console.log("we gud")
+		} else {
+		$("#invalidEmail").show();
+		$("input#email").focus();
+	    return false;	
+		}
+	} else {
+		$("#invalidEmail").show();
+		$("input#email").focus();
+	    return false;
+	};
+
+    if (!itsOkay){
+		startloc = $("input[name=startloc]").val();
+		console.log("start given: "+ startloc);
+	      if (startloc === "") {
+	        $("#invalidstart").show();
+	        $("input#startloc").focus();
+	        return false;
+	      }
+	}
+
+
+};
+
+
 
